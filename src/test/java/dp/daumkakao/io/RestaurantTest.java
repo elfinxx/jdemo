a0ab0fe184684aa10c4a3f9d31c7982c92a8c835
bart.lee <bart.lee@daumkakao.com>
2015-09-10 15:01:30 +0900

    리포트 활성화

--- src/test/java/dp/daumkakao/io/RestaurantTest.java
+++ src/test/java/dp/daumkakao/io/RestaurantTest.java
@@ -1,18 +1,27 @@
 package dp.daumkakao.io;
 
 import org.junit.Before;
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RestaurantTest {
 
     private Restaurant restaurant;
+    
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         restaurant = new Restaurant();
     }
+    
+//    @org.junit.Test
+    public void testGetMenuList() throws Exception {
+        
 
-    @Test
+    }
+
+    @org.junit.Test
     public void testAddMenu() throws Exception {
         restaurant.addMenu(new Menu());
+        assertEquals(1, restaurant.getMenuList().size());
     }
 }
